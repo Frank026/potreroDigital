af7fc4799dbb6ccf78b2a80f615f42980d044d03
Frank026 <87212185+Frank026@users.noreply.github.com>
2021-11-24 19:56:31 -0300

    Add files via upload

---
+++ Proyecto-final/Js/Era-Arc.V.js
@@ -0,0 +1,44 @@
+function mostrarCarta(){
+    var imagen = document.getElementById("fondo");
+    if(imagen.src.match("on")){
+        imagen.src= "../img/f_de_cartas.jpg";
+    }else{
+        imagen.src="../img/Dragón_de_péndulo_de_ojos_anómalos.jpg";
+    }
+}
+
+function mostrarCarta2(){
+    var imagen = document.getElementById("fondo2");
+    if(imagen.src.match("on")){
+        imagen.src= "../img/f_de_cartas.jpg";
+    }else{
+        imagen.src="../img/D-d-d_armagedon_rey_de_las_fatalidades.jpg";
+    }
+}
+
+function mostrarCarta3(){
+    var imagen = document.getElementById("fondo3");
+    if(imagen.src.match("on")){
+        imagen.src= "../img/f_de_cartas.jpg";
+    }else{
+        imagen.src="../img/Samurai_superpesado_gran_benkei.jpg";
+    }
+}
+
+function mostrarCarta4(){
+    var imagen = document.getElementById("fondo4");
+    if(imagen.src.match("on")){
+        imagen.src= "../img/f_de_cartas.jpg";
+    }else{
+        imagen.src="../img/Diva-Florecida-la-Coral-Melodiosa.jpg";
+    }
+}
+
+function mostrarCarta5(){
+    var imagen = document.getElementById("fondo5");
+    if(imagen.src.match("on")){
+        imagen.src= "../img/f_de_cartas.jpg";
+    }else{
+        imagen.src="../img/Actor-abismo-superestrella.jpg";
+    }
+}
